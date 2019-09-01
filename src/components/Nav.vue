ba1ebb058ed65cf4b3d9f92e6a72521cfb7e6cca
Llewellyn <lccollins1411@gmail.com>
2019-09-01 18:01:01 +0100

    Centre Nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,21 @@
 <template>
     <header class="navbar">
-        <section class="navbar-section">
-            <router-link to="/" class="btn btn-link" tabindex="1"><span>YAML SNIPPETS</span></router-link>
-        </section>
-        <div class="menu-button" :class="{ opened: isMenuOpened }" @click="toggle">
-            <div class="line"></div>
-            <div class="line"></div>
-            <div class="line"></div>
+        <div class="navbar container">
+            <section class="navbar-section">
+                <router-link to="/" class="btn btn-link" tabindex="1"><span>YAML SNIPPETS</span></router-link>
+            </section>
+            <div class="menu-button" :class="{ opened: isMenuOpened }" @click="toggle">
+                <div class="line"></div>
+                <div class="line"></div>
+                <div class="line"></div>
+            </div>
+            <section class="navbar-section menu-items" :class="{ closed: !isMenuOpened }">
+                <router-link to="/" class="btn btn-link" tabindex="2"><span>Explore</span></router-link>
+                <router-link v-if="isLoggedIn" to="/snippet/create" class="btn btn-link" tabindex="3"><span>Create</span></router-link>
+                <router-link v-if="!isLoggedIn" to="/signin" class="btn btn-link" tabindex="4"><span>Sign In</span></router-link>
+                <a v-if="isLoggedIn" href="#" class="btn btn-link" tabindex="4" @click="signOut"><span>Sign Out</span></a>
+            </section>
         </div>
-        <section class="navbar-section menu-items" :class="{ closed: !isMenuOpened }">
-            <router-link to="/" class="btn btn-link" tabindex="2"><span>Explore</span></router-link>
-            <router-link v-if="isLoggedIn" to="/snippet/create" class="btn btn-link" tabindex="3"><span>Create</span></router-link>
-            <router-link v-if="!isLoggedIn" to="/signin" class="btn btn-link" tabindex="4"><span>Sign In</span></router-link>
-            <a v-if="isLoggedIn" href="#" class="btn btn-link" tabindex="4" @click="signOut"><span>Sign Out</span></a>
-        </section>
     </header>
 </template>
 
